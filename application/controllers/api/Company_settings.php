6a8c3f5122722f04a52c1d5c10b265623dfd5a92
Exalted 06 <exaltedsol06@gmail.com>
2026-03-23 18:43:23 +0530

    add

--- application/controllers/api/Company_settings.php
+++ application/controllers/api/Company_settings.php
@@ -1,18 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Company_settings extends CI_Controller {
+class Company_settings extends MX_Controller  {
 
     public function __construct() {
         parent::__construct();
+		
+        $this->load->model('Api_model');
 
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *'); // VERY IMPORTANT
         header('Access-Control-Allow-Methods: GET');
-        $this->load->model('Api_model');
     }
 
     public function get_company_settings() {
-		return json_encode($this->Api_model->get_all());
+		$data = $this->Api_model->get_all();
+		echo json_encode($data);
     }
 }
--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -2,12 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api_model extends CI_Model {
-	
-	public function __construct() {
-        parent::__construct();
-        //$this->load->database();
-    }
+
     public function get_all() {
-        return $this->db->get(COMPANY_SETTINGS)->result_array();
+        return $this->db->get(COMPANY_SETTINGS)->row_array();
     }
 }
